a9e2dcf4e66f72870cc711fa9d75d118c6de55dd
Filype Abreu <62102835+zfrekey@users.noreply.github.com>
2025-03-10 10:04:32 -0300

    deps: fastify cookies added

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,38 @@
-import fastify from "fastify";
-import { ZodError } from "zod";
-import fastifyJwt from "@fastify/jwt";
-import fastifyCors from "@fastify/cors";
+import fastify from 'fastify'
+import fastifyCookie from '@fastify/cookie'
+import { z } from 'zod'
+import { userRoutes } from './http/controllers/users/routes'
+import { env } from './env'
+import fastifyCors from '@fastify/cors'
 
-export const app = fastify();
+
+export const app = fastify()
+
+app.register(require('@fastify/jwt'), {
+    secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false
+    },
+    sign: {
+        expiresIn: '10m'
+    }
+})
+
+app.register(fastifyCookie, {})
 
 app.register(fastifyCors, {
     origin: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-});
+})
 
-const jwtSecret = process.env.JWT_SECRET;
-
-if (!jwtSecret) {
-    throw new Error("JWT_SECRET is not defined in the environment variables.");
-}
-
-app.register(fastifyJwt, {
-    secret: jwtSecret, 
-    cookie: {
-        cookieName: "refreshToken",
-        signed: false,
-    },
-    sign: {
-        expiresIn: "10m"
-    }
-});
+app.register(userRoutes)
 
 app.setErrorHandler((error, request, reply) => {
-    if (error instanceof ZodError) {
-        return reply.status(400).send({ message: "Validation Error", issues: error.format() });
+    if (error instanceof z.ZodError) {
+        return reply.status(400).send({ message: 'Validation error', issues: error.format() })
     }
-    return reply.status(500).send({ message: "Internal Server Error" });
-});
+    return reply.status(500).send({ message: 'Internal server error' })
+})

891e0acc6472f08ff38f1f411210a3d76d2bd14c
Filype Abreu <62102835+zfrekey@users.noreply.github.com>
2025-03-10 10:05:54 -0300

    refactor: changed variabels

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,9 @@ import { app } from "./app"
 import { env } from "src/env/index";
 
 
-
-
 app.listen({
     host: "0.0.0.0",
     port: env.PORT
 }).then(() => {
-    console.log("Servidor está rodando na porta: http://localhost:3333")
+    console.log(`Servidor está rodando na porta: http://localhost:${env.PORT}`)
 })

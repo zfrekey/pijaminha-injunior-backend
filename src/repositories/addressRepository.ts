bbb10d68e3fdc5bcc9bf6e1d4b9363e90fceb17c
Filype Abreu <62102835+zfrekey@users.noreply.github.com>
2025-03-11 21:23:46 -0300

    refactor(adrressRepository):

--- src/repositories/addressRepository.ts
+++ src/repositories/addressRepository.ts
@@ -1,6 +1,14 @@
 import { Address, Prisma } from "@prisma/client";
 
- 
+export interface AddressUpdateInput {
+    zip_code?: string
+    state?: string
+    city?: string
+    neighborhood?: string
+    address?: string
+    street?: string
+    number?: string
+}
 
 export interface AddressRepository {
     create(data: Prisma.AddressUncheckedCreateInput): Promise<Address>

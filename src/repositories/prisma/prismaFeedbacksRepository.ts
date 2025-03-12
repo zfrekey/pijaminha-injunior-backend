80f80e7ef53468670f567773e172437dbf1724b7
Filype Abreu <62102835+zfrekey@users.noreply.github.com>
2025-03-11 22:05:48 -0300

    refactor: create function

--- src/repositories/prisma/prismaFeedbacksRepository.ts
+++ src/repositories/prisma/prismaFeedbacksRepository.ts
@@ -1,22 +1,22 @@
-import { PrismaClient, Feedback } from "@prisma/client";
+import { Feedback, Prisma } from "@prisma/client";
 import { FeedbackRepository } from "../feedbacksRepository";
 import { prisma } from "@/http/lib/prisma";
 
 
 
 export class PrismaFeedbackRepository implements FeedbackRepository {
-    async create(data: { name: string; description: string; rating: number }): Promise<Feedback> {
+    async create(data: Prisma.FeedbackUncheckedCreateInput): Promise<Feedback> {
         const feedback = await prisma.feedback.create({ data })
         return feedback
     }
 
     async findById(id: string): Promise<Feedback | null> {
-        const feedback = await prisma.feedback.findUnique({ where: { id: id } });
+        const feedback = await prisma.feedback.findUnique({ where: { id } });
         return feedback
     }
 
     async delete(id: string): Promise<void> {
-        const feedback = await prisma.feedback.findUnique({ where: { id: id } });
+        const feedback = await prisma.feedback.findUnique({ where: { id } });
         await feedback
     }
 }
